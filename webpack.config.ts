7823a9f1544188a7e3de0e7fd52225f2dad00c5a
Yauheni <yauheniauchynnikau19@gmail.com>
2023-10-02 21:56:19 +0300

    add css settings

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,7 +4,7 @@ import {BuildEnv, BuildMode, BuildPaths} from "./config/build/types/config";
 
 export default (env: BuildEnv) => {
     const options: BuildPaths = {
-        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
         output: path.resolve(__dirname, 'dist'),
         html: path.resolve(__dirname, 'public', 'index.html')
     }
