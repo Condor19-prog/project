99b25e1139b825d0e258281b0322fda0c9ea53cd
Yauheni <yauheniauchynnikau19@gmail.com>
2023-10-10 00:19:02 +0300

    themization navlink

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import './styles/index.scss'
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widget/navbar";
 
 
 export const App = () => {
@@ -11,10 +11,9 @@ export const App = () => {
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Main</Link>
-            <Link to={'/about'}>About</Link>
+            <Navbar/>
             <AppRouter/>
+            <button onClick={toggleTheme}>TOGGLE</button>
         </div>
     );
 };
