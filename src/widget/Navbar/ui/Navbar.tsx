dd344c5231118b35333530e41af67c938e02f7db
Yauheni <yauheniauchynnikau19@gmail.com>
2023-10-11 22:24:36 +0300

    add translations

--- src/widget/Navbar/ui/Navbar.tsx
+++ src/widget/Navbar/ui/Navbar.tsx
@@ -3,17 +3,19 @@ import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Navvbar.module.scss'
 import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
 import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
+import {useTranslation} from "react-i18next";
 
 interface NavbarProps {
     className?: string;
 }
 
 export const Navbar = ({className}: NavbarProps) => {
+    const {t} = useTranslation('navbar')
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
             <div className={cls.links}>
-                <AppLink to={'/'} className={cls.mainLink} theme={AppLinkTheme.SECONDARY}>Main</AppLink>
-                <AppLink to={'/about'} theme={AppLinkTheme.RED}>About</AppLink>
+                <AppLink to={'/'} className={cls.mainLink} theme={AppLinkTheme.SECONDARY}>{t('MainPage')}</AppLink>
+                <AppLink to={'/about'} theme={AppLinkTheme.RED}>{t(t('AboutPage'))}</AppLink>
             </div>
         </div>
     );
