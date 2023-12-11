b0815dc77ddebea19ed1697ccdbd3cef502dc5b7
Yauheni <yauheniauchynnikau19@gmail.com>
2023-12-11 18:55:55 +0300

    add jest

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,8 +1,7 @@
 type Mods = Record<string, boolean | string>
 
 export const classNames = (cls: string, mods: Mods = {}, additional: string[] = []) => {
-    debugger
-    return [cls, ...additional.filter(Boolean), Object.entries(mods)
+    return [cls, ...additional.filter(Boolean), ...Object.entries(mods)
         .filter(([className, value]) => Boolean(value))
         .map(([className, value]) => className)]
         .join(' ')
